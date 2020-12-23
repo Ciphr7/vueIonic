f095d21f0119b106bc0422d9f04eed94d06d3487
Ciphr7 <gil@ciphr7.com>
2020-12-23 11:43:52 -0600

    initial post

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All Memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+          <ion-item>Gil</ion-item>
+           <ion-item>Yvett</ion-item>
+            <ion-item>Jason</ion-item>
+             <ion-item>Alex</ion-item>
+              <ion-item>Heysie</ion-item>
+               <ion-item>Aaron</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonContent,
+    IonList,
+    IonItem,
+  },
+};
+</script>
